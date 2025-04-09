4f7a5efacf2c27196212ab828740a238c076e509
AryanAgrahari07 <aryanagrahari52666@gmail.com>
2025-04-09 11:57:25 +0530

    cookies setting updated

--- backend/src/controllers/verifyOTP/verifyOTP.js
+++ backend/src/controllers/verifyOTP/verifyOTP.js
@@ -190,20 +190,18 @@ exports.verifyOTP = async (req, res) => {
         }
 
         res.cookie("sessionid", sessionId, {
-          httpOnly: false, // Allow JavaScript access in development
-          secure: false, // Allow non-HTTPS in development
-          sameSite: "Lax", // Allow cross-site cookies
-          domain: "localhost", // Explicitly set domain
-          path: "/",
+          httpOnly: true, // Allow JavaScript access in development
+          secure: true, // Allow non-HTTPS in development
+          sameSite: "Strict", // Allow cross-site cookies
+          domain: "misadmindev.sundarammutual.com", // Explicitly set domain
           maxAge: 12 * 60 * 60 * 1000, // 12 hours
         });
 
         res.cookie("refreshtoken", refreshToken, {
-          httpOnly: false, // Allow JavaScript access in development
-          secure: false, // Allow non-HTTPS in development
-          sameSite: "Lax", // Allow cross-site cookies
-          domain: "localhost", // Explicitly set domain
-          path: "/",
+          httpOnly: true, // Allow JavaScript access in development
+          secure: true, // Allow non-HTTPS in development
+          sameSite: "Strict", // Allow cross-site cookies
+          domain: "misadmindev.sundarammutual.com", // Explicitly set domain
           maxAge: 20 * 60 * 1000, // 20 minutes
         });
 
